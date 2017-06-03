5a6e1c39494d882d156dd999533e1561dd94152a
fullstack412 <dt897867@gmail.com>
2017-06-03 17:45:24 +0700

    clear code

--- auth_servise/config/routes/passport.js
+++ auth_servise/config/routes/passport.js
@@ -2,8 +2,6 @@ import { AsyncRouter } from 'express-async-router'
 import passportInstagram from 'api/services/passport/instagramm'
 import { Instagram } from "api/resourses"
 
-console.log(Instagram)
-
 export default (ctx) => {
 
   const api = AsyncRouter()
@@ -12,22 +10,16 @@ export default (ctx) => {
 
   // api.get('/auth/google/callback', ctx.passport.authenticate(
   //   'google',
-
   //   { failureRedirect: '/login' },
-
   //   function(req, res) {
   //     return res.redirect('/');
   //   }),
   // )
 
+  // api.get('/auth/google', ctx.passport.authenticate('google', { scope: 'profile email' }))
 
   api.get('/instagram',
-    passportInstagram.authenticate('instagram'), (req, res) => {
-      // console.log("route")
-      // res.json({
-      //   req: req,
-      // })
-    }
+    passportInstagram.authenticate('instagram'), (req, res) => {}
   )
 
   api.get(
@@ -36,12 +28,6 @@ export default (ctx) => {
     Instagram.index,
   )
 
-
-
 	return api
 }
-
-  // api.get('/auth/google', ctx.passport.authenticate('google', { scope: 'profile email' }))
-
-
 
--- insta_servise/api/resourses/users.js
+++ insta_servise/api/resourses/users.js
@@ -1,13 +1,4 @@
-// import { pick } from 'lodash'
-// import { parseUser } from "api/services/vk"
-// import { User, Tag, ItemTag } from "api/models"
 import ig from "config/initialize/instagram"
-// console.log(Instagram)
-
-// const params = function(req) {
-//   return pick(req.body, [
-//   ])
-// }
 
 export default (context) => {
 
@@ -15,93 +6,12 @@ export default (context) => {
 
     async index(req, res, next) {
       try {
-        // let users = await User.findAll(createQuery(req, { tag: Tag }))
-        // res.json(users)
-
-
-        // ig.user('user_id', function(err, result, remaining, limit) {
-
-        //   console.log(11111)
-
-        // })
-
-        // ig.user_self_feed( (err, medias, pagination, remaining, limit) => {
-
-        //   console.log(err, medias, pagination, remaining, limit)
-
-        // });
-
-
-        ig.user('1248877206', (err, result, remaining, limit) => {
-
-          console.log(err, result, remaining, limit)
-
-        });
-
-
-        // let zz = ig.user_search('niten2niten2', (err, users, remaining, limit) => {
-        //   return new Promise((resolve, reject) => {
-        //     if (!err) {
-        //       resolve(users, remaining, limit)
-        //     } else {
-        //       reject(err)
-        //     }
-        //   })
-        // })
-
-        // let ee = await zz
-
-        // console.log(ee)
-
-        // res.json(err, users).status(200)
-
-        res.json({dsdsfsd: 3333})
-
+        res.json({test: 3333})
       } catch(error) {
         console.log(error)
         res.json({ "error": error }).status(422)
       }
     },
-
-    // async show(req, res) {
-    //   try {
-    //     let user = await User.findById(req.params.id)
-    //     res.send(user)
-    //   } catch(error) {
-    //     res.status(422).json({"error": error })
-    //   }
-    // },
-
-    // async create(req, res) {
-    //   try {
-    //     let user = await parseUser(req.body.url)
-    //     user.addTag(req.body.tag_id)
-    //     res.send(user)
-    //   } catch(error) {
-    //     res.status(422).json({"error": error })
-    //   }
-    // },
-
-    // async update(req, res) {
-    //   try {
-    //     let user = await User.findById(req.params.id)
-    //     await user.update(params(req))
-    //     res.send(user)
-    //   } catch(error) {
-    //     res.status(422)
-    //     res.json({"error": error })
-    //   }
-    // },
-
-    // async destroy(req, res) {
-    //   try {
-    //     const user = await User.findById(req.params.id)
-    //     await user.destroy()
-    //     res.status(204).json({ "message": "ok" })
-    //   } catch(error) {
-    //     res.status(422).json({"error": error })
-    //   }
-    // },
 
   }
 
