43026f7efc3fa8a6f6dbbe806f25c9f8a89c5d80
fullstack412 <dt897867@gmail.com>
2017-06-20 22:06:26 +0700

    add mobile servise

--- note.js
+++ note.js
@@ -1,2 +1,26 @@
-http://localhost:5000/v1/web-drivers/follow-explore
-http://localhost:5000/
+// http://localhost:5000/v1/web-drivers/follow-explore
+// http://localhost:5000/
+
+const test = (first, second, three) => {
+  // console.log(1111)
+  console.log(first, second, three)
+}
+
+
+function MakeSpeckledBall( ball ) {
+  // console.log(arguments)
+    // var function_name = "draw";
+    // var prev_func = ball[function_name]
+  console.log("MakeSpeckledBall")
+
+    // ball[function_name] = function () {
+        ball.apply(this, arguments)
+        // console.log("and with dots!")
+    // }
+
+    return ball;
+}
+
+
+MakeSpeckledBall(test, 3333, 444)
+
