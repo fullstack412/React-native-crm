353cce31748cbfda8d36096d4847938110590159
fullstack412 <dt897867@gmail.com>
2017-07-24 14:49:08 +0700

    fix note

--- web_servise/src/components/crm/clients/list.js
+++ web_servise/src/components/crm/clients/list.js
@@ -105,7 +105,7 @@ class ListClient extends Component {
                 <ul className="pagination">
                   <li className="page-item"><a className="page-link">Prev</a></li>
                   <li className="page-item active">
-                    <a className="page-link" href="#">1</a>
+                    <a className="page-link">1</a>
                   </li>
                   <li className="page-item"><a className="page-link">2</a></li>
                   <li className="page-item"><a className="page-link">3</a></li>
