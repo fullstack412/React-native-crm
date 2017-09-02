9a64657ab8995a071733ff565eccdd72e37d1ea5
fullstack412 <dt897867@gmail.com>
2017-09-02 10:38:20 +0700

    change

--- api_gateway/api/graphql/services/api/resolvers.spec.js
+++ api_gateway/api/graphql/services/api/resolvers.spec.js
@@ -11,20 +11,21 @@ describe('api resolvers', () => {
   it("users", async () => {
     // console.log(ApiQuery.users)
 
+    console.log(22)
 
-    console.log(111)
-    console.log(111)
-    console.log(111)
-    console.log(111)
-    console.log(111)
-    console.log(111)
+    // console.log(111)
+    // console.log(111)
+    // console.log(111)
+    // console.log(111)
+    // console.log(111)
+    // console.log(111)
     // console.log(isAuthenticated)
-
-    throw new Error("test")
+    // throw new Error("test")
     // let resp = await ApiQuery.users()
     // console.log(resp)
 
 
+    expect(111).to.be.a('string')
 
 
 
