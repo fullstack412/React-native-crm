50fa13ab7af57ef5b0af8581efc0d4633e40561a
fullstack412 <dt897867@gmail.com>
2017-09-08 11:27:00 +0700

    add support

--- web/src/components/shared/header/index.stories.js
+++ web/src/components/shared/header/index.stories.js
@@ -1,14 +1,13 @@
-import React from 'react';
+import React from 'react'
 import { storiesOf } from '@storybook/react'
-import { action } from '@storybook/addon-actions'
+import { withProvider, withRouter } from 'specs/support'
 import App from './index'
-import { Provider } from 'react-redux'
-import { configureStore } from 'store'
-import { MemoryRouter } from 'react-router-dom'
 
 const stateIsLogin = { settings: { login: true } }
 
 storiesOf('header', module)
-  .addDecorator(story => <MemoryRouter>{story()}</MemoryRouter>)
-  .add('with login', () => (<Provider store={configureStore(stateIsLogin)}><App/></Provider>))
-  .add('without login', () => (<Provider store={configureStore()}><App/></Provider>))
+  .addDecorator(story => withRouter(story()))
+  .add('with login', () => (withProvider(<App />, stateIsLogin)))
+  .add('without login', () => (withProvider(<App />)))
+
+
