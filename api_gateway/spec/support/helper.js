2c8835efe400f18c75b43b0859191d2a7a464f63
fullstack412 <dt897867@gmail.com>
2017-09-19 22:04:46 +0700

    clear code

--- api_gateway/spec/support/helper.js
+++ api_gateway/spec/support/helper.js
@@ -1,52 +1,13 @@
 import { createApolloFetch } from 'apollo-fetch'
-// import jsonwebtoken from 'jsonwebtoken'
 import settings from 'config/settings'
 import { createJwt } from "api/services/jwt"
-// import jsonwebtoken from 'jsonwebtoken'
-
-const uri = `http://localhost:${settings.port}/v1`
-const apolloFetch = createApolloFetch({ uri })
-
-
-// const createJwtTest = () => {
-//   return jsonwebtoken.sign(
-//     {
-//       user_id: 1,
-//       email: "test@test.com",
-//     },
-//     settings.jwt_secret_key,
-//     {
-//       expiresIn: 10000000000
-//     }
-//   )
-// }
-
-
 
 export const graphqlQuery = (query, variables, user) => {
 
+  const uri = `http://localhost:${settings.port}/v1`
+  const apolloFetch = createApolloFetch({ uri })
+
   if (user) {
-    // console.log(user)
-
-    // const middleware = ({ request }, next) => {
-    //   if (!request.options.headers) {
-    //     request.options.headers = {}
-    //   }
-    //   request.options.headers.authorization = `Token ${createJwt(user)}`
-    //   next()
-    // }
-
-    // const middleware = ({ req }, next) => {
-
-    //   if (!req.options.headers) {
-    //     req.options.headers = {}
-    //   }
-    //   req.options.headers.authorization = `Token ${createJwt(user)}`
-    //   next()
-    // }
-
-    // apolloFetch.use(middleware)
-
     apolloFetch.use(({ request, options }, next) => {
       if (!options.headers) {
         options.headers = {}
@@ -55,7 +16,6 @@ export const graphqlQuery = (query, variables, user) => {
 
       next()
     })
-
   }
 
   return apolloFetch({ query, variables })
