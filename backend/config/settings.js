c72e393e18e79df6e53ebe77060f972df05077f1
fullstack412 <dt897867@gmail.com>
2018-04-26 12:55:05 +0700

    add travis

--- backend/config/settings.js
+++ backend/config/settings.js
@@ -11,13 +11,17 @@ const getPath = () => {
 dotenv.config({ path: getPath() })
 
 export default {
-  name: "api_gateway",
   env: process.env.NODE_ENV,
-  isEnvTest: process.env.NODE_ENV == "test",
-  storage: `./db/database.${process.env.NODE_ENV}.sqlite`,
+  name: process.env.APP_NAME,
   port: process.env.PORT || 3000,
+
+  isEnvTest: process.env.NODE_ENV == "test",
+
+  storage: `./db/database.${process.env.NODE_ENV}.sqlite`,
+
   jwt_secret_key: process.env.JWT_SECRET_KEY,
-  crmUri: process.env.CRM_URI,
-  vkUri: process.env.VK_URI,
-  instaUri: process.env.INSTA_URI,
+
+  // crmUri: process.env.CRM_URI,
+  // vkUri: process.env.VK_URI,
+  // instaUri: process.env.INSTA_URI,
 }
