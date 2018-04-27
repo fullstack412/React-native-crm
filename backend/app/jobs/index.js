12b43ab9e29090d1c2acfeec65ca36536adb053f
fullstack412 <dt897867@gmail.com>
2018-04-27 11:48:55 +0700

    add shedule

--- backend/app/jobs/index.js
+++ backend/app/jobs/index.js
@@ -1,10 +1,9 @@
 import schedule from 'node-schedule'
 import { andPersonInFriend } from "app/services/vk/methods"
-import { getRandomArbitrary } from "app/services/utils"
+import { getRandomInt } from "app/services/utils"
 
 const buildRule = () => {
-  const number = getRandomArbitrary(1, 3)
-  console.log(number)
+  const number = getRandomInt(5, 10)
 
   let rule = new schedule.RecurrenceRule()
   rule.minute = new schedule.Range(0, 59, number)
@@ -13,8 +12,7 @@ const buildRule = () => {
 }
 
 const run = async () => {
-  console.log(11111)
-  // await andPersonInFriend()
+  await andPersonInFriend()
 }
 
 schedule.scheduleJob(buildRule(), async () => { await run() })
--- backend/app/services/utils.js
+++ backend/app/services/utils.js
@@ -1,3 +1,5 @@
 export const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
 
 export const getRandomArbitrary = (min, max) => Math.random() * (max - min) + min
+
+export const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min)) + min
