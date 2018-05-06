516593647901097bf9409be562a6c3b2889a40c0
fullstack412 <dt897867@gmail.com>
2018-05-06 23:21:55 +0700

    update front

--- backend/app/jobs/index.js
+++ backend/app/jobs/index.js
@@ -3,15 +3,20 @@ import { andPersonInFriendFirstUserWithLimit } from "app/services/vk/methods"
 import { getRandomInt } from "app/services/utils"
 
 const buildRule = () => {
-  const number = getRandomInt(50, 59)
+  // const number = getRandomInt(50, 59)
+  const number = getRandomInt(58, 59)
 
   let rule = new schedule.RecurrenceRule()
   rule.minute = new schedule.Range(0, 59, number)
+
+  console.log(rule)
 
   return rule
 }
 
 const run = async () => {
+  console.log("run andPersonInFriendFirstUserWithLimit")
+
   await andPersonInFriendFirstUserWithLimit()
 }
 
