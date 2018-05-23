9bcf4ebd4270abce00109aba09952f67582b3bec
fullstack412 <dt897867@gmail.com>
2018-05-23 17:31:26 +0700

    edit logger

--- README.md
+++ README.md
@@ -4,7 +4,20 @@
 
 - install docker
 - docker-compose build
+
+## Docker
+
+### development
 - docker-compose up
+
+### production
+- docker-compose -f docker-compose.production.yml build
+
+### production job
+- docker-compose -f docker-compose.production.job.yml build
+
+
+
 
 ### Get token vk for example
 https://oauth.vk.com/authorize?client_id=CLIENT_ID&scope=friends,photos,audio,video,docs,notes,pages,status,offers,questions,wall,groups,messages,email,notifications,stats,ads,offline,docs,pages,stats,notifications&response_type=token
--- backend/app/services/logger.js
+++ backend/app/services/logger.js
@@ -15,6 +15,20 @@ const getStreams = () => {
   ]
 }
 
+const getStreamsJob = () => {
+  if (settings.isEnvTest) return []
+
+  return [
+    {
+      level: 'trace',
+      stream: process.stdout
+    },
+    {
+      path: process.cwd() + `/log/${settings.env}.job.log`,
+    },
+  ]
+}
+
 const buildLogger = () => {
   return bunyan.createLogger({
 
@@ -30,4 +44,22 @@ const buildLogger = () => {
   })
 }
 
-export default buildLogger()
+const buildJobLogger = () => {
+  return bunyan.createLogger({
+
+    name: settings.env,
+    level: 'trace',
+
+    serializers: {
+      req: bunyan.stdSerializers.req,
+      res: bunyan.stdSerializers.res
+    },
+
+    streams: getStreamsJob(),
+  })
+}
+
+const logger = buildLogger()
+export const loggerJob = buildJobLogger()
+
+export default logger
