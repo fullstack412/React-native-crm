d2d553d0ab93060b91afe461f1fc5ee428e02ddc
fullstack412 <dt897867@gmail.com>
2018-06-09 15:01:38 +0700

    clear code

--- backend/app/job/queue.js
+++
@@ -1,5 +0,0 @@
-// import Queue from "bull"
-// import settings from "config/settings"
-
-// export const vkPersonsQueue = new Queue("vkPersonsQueue", settings.redisUrl)
-// export const vkFriendsQueue = new Queue("vkFriendsQueue", settings.redisUrl)
